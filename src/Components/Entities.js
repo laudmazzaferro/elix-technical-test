a6d3df78c1cbbf7d35e7574cd9ceba8739676542
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-10-18 00:25:56 +0200

    add style to entities list

--- src/Components/Entities.js
+++ src/Components/Entities.js
@@ -5,45 +5,48 @@ import { Link } from 'react-router-dom';
 
 
 const Entities = props => {
-  const { routerProps ,entities ,menuCollapsible ,idCollap ,printCode} = props;
-  const assetId = parseInt(routerProps.match.params.assetId); 
-  const entitiesDos = entities.filter(item => item.id_asset === assetId );
-  
+  const { routerProps, entities, menuCollapsible, idCollap, printCode } = props;
+  const assetId = parseInt(routerProps.match.params.assetId);
+  const entitiesDos = entities.filter(item => item.id_asset === assetId);
+
   if (entitiesDos[0]) {
-   
-  return (
-    <ul className="entities-container">
-      {entitiesDos.map(item => {
-        return(
-          <li className="entities-item_list">
-            <div className="container-ent-item_list">
-              <div className="box-ent-item_list" onClick={menuCollapsible} id={item.id}>
-                <h2 className="title-ent-item_list" >Entry code:  {item.t_entry_code}  <i  className='fas fa-chevron-down'></i> 
-                </h2>
-                <p className="ent-area">Interior area: {item.n_entry_area_gross_interior} m2</p>
-                <p className="ent-tenant-name">Tenant name: {item.t_entry_tenant_name} </p>
-              </div>
-              {/*<ContextMenu contextId={item.id}  items={[{label:item.t_entry_code , onClick:printCode}]} />*/}
-              <div className={`collapsible ${(parseInt(item.id) === parseInt(idCollap)) ? '' : 'hidden'}`}>
-                <button className="btn-print" onClick={printCode} id={item.t_entry_code}>
-                Print on Console
+
+    return (
+      <React.Fragment>
+        <div className="entities-container">
+          <ul className="entities-list">
+            {entitiesDos.map(item => {
+              return (
+                <li className="entities-item_list">
+                  <div className="container-ent-item_list">
+                    <div className="box-ent-item_list" onClick={menuCollapsible} id={item.id}>
+                      <h2 className="title-ent-item_list" >Entry code:  {item.t_entry_code}  <i className={`fas fa-chevron-down ${(parseInt(item.id) === parseInt(idCollap)) ? 'hidden' : ''}`}></i>
+                      </h2>
+                      <p className="ent-area">Interior area: {item.n_entry_area_gross_interior} m2</p>
+                      <p className="ent-tenant-name">Tenant name: {item.t_entry_tenant_name} </p>
+                    </div>
+                    {/*<ContextMenu contextId={item.id}  items={[{label:item.t_entry_code , onClick:printCode}]} />*/}
+                    <div className={`collapsible ${(parseInt(item.id) === parseInt(idCollap)) ? '' : 'hidden'}`}>
+                      <button className="btn-print" onClick={printCode} id={item.t_entry_code}>
+                        Print on Console
                 </button>
-              </div>
-            </div>
-          </li>
-        )
-      })}
-      <Link to={`/`} className="charapter-link">
-        <button>Volver</button>
-      </Link>
-  
-    </ul>
-  )
-  }else{
+                    </div>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <Link to={`/`} className="charapter-link">
+          <button>Return</button>
+        </Link>
+      </React.Fragment>
+    )
+  } else {
     return (
       <React.Fragment>
         <p>Entidades no existentes</p>
-        <Link to="/" >Volver </Link>
+        <Link to="/" >Return </Link>
       </React.Fragment>
     )
   }
