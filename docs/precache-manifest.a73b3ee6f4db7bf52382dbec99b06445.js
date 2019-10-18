5e486326039b4a19c867a6e5f457dbe7216bce47
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-10-18 13:57:56 +0200

    build docs

---
+++ docs/precache-manifest.a73b3ee6f4db7bf52382dbec99b06445.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8021cfcaec97f8eb953f0776a158753",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a7e2f3214fd54930c2eb",
+    "url": "./static/css/main.665c0e3d.chunk.css"
+  },
+  {
+    "revision": "41fbb1c124c3cb6989e7",
+    "url": "./static/js/2.f0510bdd.chunk.js"
+  },
+  {
+    "revision": "a7e2f3214fd54930c2eb",
+    "url": "./static/js/main.5fa4633a.chunk.js"
+  },
+  {
+    "revision": "37a826aa546dab09d1ff",
+    "url": "./static/js/runtime-main.b9c7f1bb.js"
+  }
+]);
